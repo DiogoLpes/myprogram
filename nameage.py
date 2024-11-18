73bbd2fae45770c534a9ed928bc721f99154da56
Diogo Lopes <diogolopes285@gmail.com>
2024-11-18 21:33:02 +0000

    secondcommit

---
+++ nameage.py
@@ -0,0 +1,12 @@
+nome = input("Qual é o seu nome? ")
+
+
+print(f"Olá, {nome}! Bem-vindo ao mundo da programação Python!")
+
+idade = input("Qual é a sua idade? ")
+
+
+idade = int(idade)
+
+
+print(f"Sua idade é {idade} anos.")
